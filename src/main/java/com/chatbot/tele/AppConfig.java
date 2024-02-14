1b36b33fae0e9df86ecb2832e82d4ab90e0ecdbc
MetinovAdik <metinovadilet@gmail.com>
2024-02-14 17:43:49 +0600

    Translates into kyrgyz text

--- src/main/java/com/chatbot/tele/AppConfig.java
+++ src/main/java/com/chatbot/tele/AppConfig.java
@@ -5,14 +5,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
+
 @Configuration
 public class AppConfig {
 
     @Bean
     public OkHttpClient okHttpClient() {
-        return new OkHttpClient();
+        return new OkHttpClient.Builder()
+                .connectTimeout(Duration.ofSeconds(30000)) // 30 seconds in milliseconds
+                .readTimeout(Duration.ofSeconds(30000))    // 30 seconds in milliseconds
+                .writeTimeout(Duration.ofSeconds(30000))   // 30 seconds in milliseconds
+                .build();
     }
-
     @Bean
     public TranslationService translationService(@Value("${openai.api.key}") String apiKey, OkHttpClient client) {
         return new TranslationService(apiKey, client);
